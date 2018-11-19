fc41b6bfd7c899eb348c21530b0a7f563173fbeb
Richard Quinlivan <rquinli1@binghamton.edu>
2018-11-19 14:30:56 -0500

    removed redundant files

--- src/Hero.py
+++ src/Hero.py
@@ -1,27 +1,29 @@
 import pygame
 
 class hero(pygame.sprite.Sprite):
-        def __init__(self, name, pos, image):
-            pygame.sprite.Sprite.__init__(self)
-            self.name = name
-            self.image = pygame.image.load(image)
-            self.rect = get.image.get_rect()
-            self.rect.x = pos[0] #don't need if start at origin
-            self.rect.y = pos[1] #don't need if start at origin
-            self.strength = 1
-            self.speed = 1
+	def __init__(self, name, pos, image):
+		pygame.sprite.Sprite.__init__(self)
+		self.name = name
+		self.image = pygame.image.load(image)
+		self.rect = get.image.get_rect()
+		self.rect.x = pos[0] #don't need if start at origin
+		self.rect.y = pos[1] #don't need if start at origin
+		self.strength = 1
+		self.speed = 1
 
-        def move(self, direction):
-            if (direction.lower() == "down"):
-                self.rect.y -= self.speed
-            if (direction.lower() == "up"):
-                self.rect.y += self.speed
-            if (direction.lower() == "left"):
-                self.rect.x -= self.speed
-            if (direction.lower() == "right"):
-                self.rect.x += self.speed
-        def fight(self, other):
-            if (other.strength > self.strength):
-                return random.choice([True, Flase])
+	def move(self, direction):
+		if (direction.lower() == "down"):
+			self.rect.y -= self.speed
+			if (direction.lower() == "up"):
+				self.rect.y += self.speed
+			if (direction.lower() == "left"):
+				self.rect.x -= self.speed
+			if (direction.lower() == "right"):
+				self.rect.x += self.speed
+	def fight(self, other):
+		if (other.strength > self.strength):
+			return random.choice([True, Flase])
+	def take_damage(self):
+		self.health += -1
 
-            #fight is being called through controller
+			#fight is being called through controller
